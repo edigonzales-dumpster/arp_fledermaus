bb08fb6fd7999f428aca4b7b031d3a1b662ac440
Stefan Ziegler <edi.gonzales@gmail.com>
2020-08-06 16:38:26 +0200

    remove archive

--- createschema_pub/Archive/ili2pg_createschema.sh
+++
@@ -1,4 +0,0 @@
-java -jar /usr/local/ili2pg-4.3.1/ili2pg.jar \
---dbschema arp_fledermaus_pub --models SO_ARP_Fledermausfundorte_Publikation_20200806 \
---defaultSrsCode 2056 --createGeomIdx --createFk --createFkIdx --createEnumTabs --beautifyEnumDispName --createMetaInfo --createNumChecks --nameByTopic --strokeArcs \
---createscript arp_fledermaus_pub.sql
